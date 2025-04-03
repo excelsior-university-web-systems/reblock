0adafbf0f4d17af4946525324de5766281acfddb
Lin87 <ethan.lin.05@gmail.com>
2025-04-03 17:28:22 -0500

    Fix powerscript

--- postprocess_zip.ps1
+++ postprocess_zip.ps1
@@ -4,17 +4,21 @@ $TEMP_DIR = "temp"
 $PLUGIN_FOLDER = "reblock"
 
 # Create temporary directory if it doesn't exist
+Write-Output "Create temporary directory if it doesn't exist."
 if (-Not (Test-Path $TEMP_DIR)) {
     New-Item -Path $TEMP_DIR -ItemType Directory | Out-Null
 }
 
 # Extract the zip file into the temporary directory
+Write-Output "Extract the zip file into the temporary directory."
 Expand-Archive -Path $ZIP_FILE -DestinationPath $TEMP_DIR -Force
 
 # Remove the original zip file
+Write-Output "Remove the original zip file."
 Remove-Item -Path $ZIP_FILE -Force
 
 # Remove package.json and README.md if they exist in the plugin folder
+Write-Output "Remove package.json and README.md if they exist in the plugin folder."
 $pkgFile = Join-Path $TEMP_DIR "$PLUGIN_FOLDER\package.json"
 if (Test-Path $pkgFile) {
     Remove-Item -Path $pkgFile -Force
@@ -26,9 +30,11 @@ if (Test-Path $readmeFile) {
 }
 
 # Repackage the contents into a new zip file
+Write-Output "Repackage the contents into a new zip file."
 Push-Location $TEMP_DIR
 Compress-Archive -Path $PLUGIN_FOLDER -DestinationPath "..\$ZIP_FILE" -Force
 Pop-Location
 
 # Clean up temporary directory
+Write-Output "SUCCESS! Clean up temporary directory."
 Remove-Item -Path $TEMP_DIR -Recurse -Force
