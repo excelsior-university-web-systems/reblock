292605649ccca7c9715100c98d30a98715661683
Ethan Lin <ethan.lin.05@gmail.com>
2025-04-03 17:10:26 -0500

    compiled the block build folder

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '34ba1a0925a37cb09b32');
