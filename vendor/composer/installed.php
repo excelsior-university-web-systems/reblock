9a41e4feb904f5c1437ed250ccc7263bd4bce98f
Ethan Lin <ethan.lin.05@gmail.com>
2025-04-03 17:09:27 -0500

    install composer packages and autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'eslin87/reblock',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0c320974edf18f1c82528af28e073d89480880b6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eslin87/reblock' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0c320974edf18f1c82528af28e073d89480880b6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
