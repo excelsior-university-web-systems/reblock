f658199eb7e165d6685a8142f852da9601f49abf
Ethan Lin <ethan.lin.05@gmail.com>
2025-05-28 12:14:35 -0500

    version 1.1.3 build

--- reblock.php
+++ reblock.php
@@ -5,7 +5,7 @@
  * Description: ReBlock creates a centralized content hub to efficiently manage common reusable content blocks, ensuring consistency, quality, and accessibility.
  * Author:      Ethan Lin
  * Author URI:  https://profiles.wordpress.org/eslin87/
- * Version:     1.1.2
+ * Version:     1.1.3
  * License:     GPLv2 or later
  */
 
